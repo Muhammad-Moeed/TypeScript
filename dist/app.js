7269f9c95c186c5c0de39fff329281cdb6b4da00
Muhammad Moeed <m.moeedq497@gmail.com>
2025-02-16 16:02:22 +0500

    Interface , Union Oops, Classes , Inheritance Class , Abstract Class ,
    Interface , Union Oops, Classes , Inheritance Class , Abstract Class ,

--- dist/app.js
+++ dist/app.js
@@ -1,39 +1,133 @@
 "use strict";
-// interface newh {
-//     id: number,
-//     name:string
-//     }
-//     let myType:newh = { id: 1, name: "Moeed" };
+// ********************Task : 01*********************
 Object.defineProperty(exports, "__esModule", { value: true });
-//     let staleObject = { id: 2, name: "Ali", age: 25 };
-//     myType = staleObject;
-//     console.log(myType)
-//     const enum Colors {
-//         Red,
-//         Green,
-//         Blue
-//       }
-//       let myColor = Colors.Green;
-//       console.log(myColor);
-//       const enum Color2 {Red = 1, Green = 2, Blue = 4};//can assign values to all
-// var colorIndex = Color2["Blue"];
-// console.log(colorIndex);
-// Classes
-class StudentInfo {
-    name;
-    position;
-    constructor(n, p) {
-        this.name = n;
-        this.position = p;
+function createUser(user) {
+    return user;
+}
+const newUser = {
+    id: 254118,
+    name: "Muhammad Moeed",
+    email: "moeed123@gmail.com",
+    isActive: true,
+};
+const checkUser = createUser(newUser);
+console.log(checkUser);
+const processInput = (input) => {
+    if (typeof input === 'number') {
+        return `Number: ${input}`;
     }
-    getName() {
-        return this.name;
+    else {
+        return `String: ${input}`;
     }
-    setName(userName) {
-        this.name = userName;
+};
+// ********************Task : 03*********************
+class Vehicle {
+    make;
+    model;
+    year;
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    getInfo() {
+        return `${this.year} ${this.make} ${this.model}`;
     }
 }
-let newStd = new StudentInfo("Ali", 2);
-newStd.setName('John');
-console.log(newStd);
+class Car extends Vehicle {
+    doors;
+    constructor(make, model, year, doors) {
+        super(make, model, year);
+        this.doors = doors;
+    }
+    getInfo() {
+        return `${super.getInfo()} ${this.doors} `;
+    }
+}
+class MotorCycle extends Vehicle {
+    hasSidecar;
+    constructor(make, model, year, hasSidecar) {
+        super(make, model, year);
+        this.hasSidecar = hasSidecar;
+    }
+    getInfo() {
+        return `${super.getInfo()} ${this.hasSidecar} `;
+    }
+}
+const car = new Car("BMW", "new", 2024, 4);
+console.log(car.getInfo());
+const motorcycle = new MotorCycle("Alto", "660cc", 2019, false);
+console.log(motorcycle.getInfo());
+// ********************Task : 04*********************
+class bankAccount {
+    isbalance = 0;
+    isAccountNumber;
+    constructor(accountNumber, Balance) {
+        this.isAccountNumber = accountNumber;
+        this.isbalance = Balance;
+    }
+    get balance() {
+        return this.isbalance;
+    }
+    get accountNumber() {
+        return this.isAccountNumber;
+    }
+    deposit(amount) {
+        this.isbalance += amount;
+        console.log(`Your deposit Amount is: ${amount}. & Total Amount now is: ${this.isbalance}`);
+    }
+    withdraw(amount) {
+        if (amount > this.isbalance) {
+            throw new Error(`Your withdraw amount is greator than, your balance is ${this.isbalance}`);
+        }
+        this.isbalance -= amount;
+        console.log(`Withdrawn: ${amount}. Remaining balance: ${this.isbalance}`);
+    }
+}
+let Account = new bankAccount('6234204', 1000);
+console.log(`Account Number: ${Account.accountNumber}`);
+console.log(`Initial Balance: ${Account.balance}`);
+// console.log(Account.isbalance) # Error 
+console.log(`Your balance is ${Account.balance}`);
+Account.deposit(800);
+Account.withdraw(500);
+console.log('Thanks You M Moeed!');
+// ********************Task : 05*********************
+class Shape {
+    color;
+    constructor(color) {
+        this.color = color;
+    }
+    getColor() {
+        return this.color;
+    }
+}
+class Circle extends Shape {
+    radius;
+    constructor(color, radius) {
+        super(color);
+        this.radius = radius;
+    }
+    calculateArea() {
+        return 3.14159 ** this.radius;
+    }
+}
+class Rectangle extends Shape {
+    width;
+    height;
+    constructor(color, width, height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+    calculateArea() {
+        return this.width * this.height;
+    }
+}
+const myCircle = new Circle("Red", 7);
+console.log(`Circle Color: ${myCircle.getColor()}`);
+console.log(`Circle Area: ${myCircle.calculateArea()}`);
+const myRectangle = new Rectangle("Blue", 8, 8);
+console.log(`Rectangle Color: ${myRectangle.getColor()}`);
+console.log(`Rectangle Area: ${myRectangle.calculateArea()}`);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,159 @@
-// interface newh {
-//     id: number,
-//     name:string
-//     }
-//     let myType:newh = { id: 1, name: "Moeed" };
-    
-//     let staleObject = { id: 2, name: "Ali", age: 25 };
-    
-//     myType = staleObject;
-//     console.log(myType)
+        // ********************Task : 01*********************
 
-//     const enum Colors {
-//         Red,
-//         Green,
-//         Blue
-//       }
-      
-//       let myColor = Colors.Green;
-//       console.log(myColor);
-
-//       const enum Color2 {Red = 1, Green = 2, Blue = 4};//can assign values to all
-// var colorIndex = Color2["Blue"];
-// console.log(colorIndex);
-
-
-// Classes
-
-class StudentInfo {
-  private name: string;
-  position: number;
-
-  constructor(n: string, p: number) {
-    this.name = n;
-    this.position = p;
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  setName(userName: string) {
-    this.name = userName;
-  }
+interface User {
+    id: number,
+    name: string,
+    email: string,
+    isActive: boolean
+}
+function createUser(user: User): User {
+    return user;
 }
 
-let newStd = new StudentInfo("Ali", 2);
-newStd.setName('John');
-console.log(newStd);
+const newUser: User = {
+    id: 254118,
+    name: "Muhammad Moeed",
+    email: "moeed123@gmail.com",
+    isActive: true,
+};
 
+const checkUser = createUser(newUser);
+console.log(checkUser);
 
+        // ********************Task : 02*********************
+
+type Input = number | string
+
+const processInput = (input: Input): string => {
+    if (typeof input === 'number') {
+        return `Number: ${input}`;
+    } else {
+        return `String: ${input}`;
+    }
+}
+
+        // ********************Task : 03*********************
+
+class Vehicle {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    getInfo(): string {
+        return `${this.year} ${this.make} ${this.model}`;
+    }
+}
+
+class Car extends Vehicle{
+    doors : number;
+
+    constructor(make: string, model: string, year: number, doors: number) {
+        super(make, model, year);
+        this.doors = doors;
+    }
+    getInfo(): string {
+        return `${super.getInfo()} ${this.doors} `; 
+      }
+
+}
+class MotorCycle extends Vehicle{
+    hasSidecar : boolean;
+
+    constructor(make: string, model: string, year: number, hasSidecar: boolean) {
+        super(make, model, year);
+        this.hasSidecar = hasSidecar;
+    }
+    getInfo(): string {
+        return `${super.getInfo()} ${this.hasSidecar} `;
+      }
+}
+
+const car = new Car("BMW", "new", 2024, 4);
+console.log(car.getInfo());
+
+const motorcycle = new MotorCycle("Alto", "660cc", 2019, false);
+console.log(motorcycle.getInfo());
+
+        // ********************Task : 04*********************
+
+class bankAccount{
+    private isbalance : number = 0;
+    readonly isAccountNumber : string;
+    
+    constructor(accountNumber: string, Balance :number) {
+        this.isAccountNumber = accountNumber;
+        this.isbalance = Balance
+    }
+    get balance(): number {
+        return this.isbalance;
+    }
+    get accountNumber(): string {
+        return this.isAccountNumber;
+    }
+    deposit(amount: number): void {
+            this.isbalance += amount;
+            console.log(`Your deposit Amount is: ${amount}. & Total Amount now is: ${this.isbalance}`);
+    }
+    withdraw(amount: number): void {
+        if (amount > this.isbalance) {
+            throw new Error(`Your withdraw amount is greator than, your balance is ${this.isbalance}`);
+        }
+        this.isbalance -= amount;
+        console.log(`Withdrawn: ${amount}. Remaining balance: ${this.isbalance}`);
+    }
+
+}
+
+let Account = new bankAccount('6234204',1000)
+console.log(`Account Number: ${Account.accountNumber}`); 
+console.log(`Initial Balance: ${Account.balance}`); 
+// console.log(Account.isbalance) # Error 
+
+console.log(`Your balance is ${Account.balance}`)
+Account.deposit(800)
+Account.withdraw(500)
+console.log('Thanks You M Moeed!')
+
+        // ********************Task : 05*********************
+
+abstract class Shape{
+    protected color: string;
+    constructor(color: string) {
+        this.color = color;
+    }
+    abstract calculateArea():number;
+    getColor(): string {
+        return this.color;
+    }  
+}
+
+class Circle extends Shape {
+    constructor(color: string, private radius: number) {
+        super(color);
+    }
+    calculateArea(): number {
+        return 3.14159 ** this.radius;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(color: string, private width: number, private height: number) {
+        super(color);
+    }
+    calculateArea(): number {
+        return this.width * this.height;
+    }
+}
+
+const myCircle = new Circle("Red", 7);
+console.log(`Circle Color: ${myCircle.getColor()}`);
+console.log(`Circle Area: ${myCircle.calculateArea()}`);
+
+const myRectangle = new Rectangle("Blue", 8, 8);
+console.log(`Rectangle Color: ${myRectangle.getColor()}`);
+console.log(`Rectangle Area: ${myRectangle.calculateArea()}`);
